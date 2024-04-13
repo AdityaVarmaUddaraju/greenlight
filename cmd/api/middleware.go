93e08ab91af3bf66da0c8b944c0b941bcbbd745d
Aditya Varma Uddaraju <adithya951@gmail.com>
2024-04-13 20:12:37 +0530

    ip based rate limit

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 
 	"golang.org/x/time/rate"
 )
@@ -22,13 +25,56 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 }
 
 func (app *application) ratelimit(next http.Handler) http.Handler {
-	limit := rate.NewLimiter(2, 4)
+
+	type client struct {
+		ratelimiter *rate.Limiter
+		lastSeen time.Time
+	}
+
+	var (
+		mu sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	go func ()  {
+		for {
+			time.Sleep(time.Minute)
+
+			mu.Lock()
+			for ip, c := range clients {
+				if time.Since(c.lastSeen) > (3 * time.Minute) {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock()
+		}
+	}()
 
 	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request)  {
-		if !limit.Allow() {
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+
+		mu.Lock()
+
+		if _, exists := clients[ip]; !exists {
+			clients[ip] = &client{
+				ratelimiter: rate.NewLimiter(2, 4),
+			}
+		}
+
+		clients[ip].lastSeen = time.Now()
+
+		if !clients[ip].ratelimiter.Allow() {
+			mu.Unlock()
 			app.ratelimitExceededResponse(w, r)
 			return
 		}
+
+		mu.Unlock()
 
 		next.ServeHTTP(w, r)
 	})
